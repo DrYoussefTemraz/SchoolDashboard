94f02449e41d8305c78626ed4953431c74eddb43
Youssef <yousseftahertemraz.com>
2025-03-01 15:32:23 +0200

    styling the left and right bars

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -7,7 +7,12 @@ export default function Dashboardlayout({
 }>) {
   return (
 
-      <body> dashboard {children}</body>
+    <div className="h-screen flex">
+      {/* left */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200"> L </div>
+      {/* right */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%]xl:w-[86%] bg-blue-200"> R </div>
+    </div>
 
   );
 }
