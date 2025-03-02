e673f3c729482ab80d6be6d97a54b7def4c66e30
Youssef <yousseftahertemraz.com>
2025-03-03 00:17:05 +0200

    styling the cards by tailwind color theme

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,20 @@
+import UserCard from "@/components/UserCard"
+
 const AdminPage = () => {
   return (
-    <div className=''>AdminPage</div>
+    <div className='p-4 flex flex-col md:flex-row'>
+      <div className="w-full md:w-2/3">
+      {/* userCards */}
+      <div className="flex gap-4 justify-between flex-wrap">
+        <UserCard type="student"/>
+        <UserCard type="teacher"/>
+        <UserCard type="parent"/>
+        <UserCard type="staff"/>
+      </div>
+      </div>
+      <div className="w-full md:w-1/3">f</div>
+
+    </div>
   )
 }
 
