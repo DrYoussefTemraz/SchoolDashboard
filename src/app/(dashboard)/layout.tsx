58f35dba5756111e44bbe0573b5ed15a29078b4b
Youssef <yousseftahertemraz.com>
2025-03-02 03:36:18 +0200

    styling the menu bar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Menu from "@/components/Menu"
+import Navbar from "@/components/Navbar";
 
 
 export default function Dashboardlayout({
@@ -12,15 +13,18 @@ export default function Dashboardlayout({
 
     <div className="h-screen flex">
       {/* left */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
         <Link href="/" className="flex items-center justify-center lg:justify-start gap-2 ">
           <Image src="/logo.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block">School Management</span>
         </Link>
-        <Menu/>
+        <Menu />
       </div>
       {/* right */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%]xl:w-[86%] bg-blue-200"> R </div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar/>
+        {children}
+      </div>
     </div>
 
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,21 @@
+import Image from "next/image"
+
 const Navbar = () => {
   return (
-    <div className=''>Navbar</div>
+    <div className='flex items-center justify-between'>
+      {/* SEARCH BAR */}
+      <div className='hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2'>
+        <Image src="/search.png" alt="" width={14} height={14}/>
+        <input type="text" placeholder="Search..." className="w-[200px] p-2 bg-transparent outline-none"/>
+      </div>
+      {/* ICONS AND USERS */}
+      <div className="">
+        <div className="">
+          <Image src="/message.png" alt="" width={20} height={20} />
+        </div>
+
+      </div>
+    </div>
   )
 }
 
