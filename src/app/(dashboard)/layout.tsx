bf17e9ea0a9daf4a268f87be52d22a16ca82148f
Youssef <yousseftahertemraz.com>
2025-03-02 02:40:02 +0200

    adding menu items and design the layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "@/components/Menu"
 
 
 export default function Dashboardlayout({
@@ -9,7 +12,13 @@ export default function Dashboardlayout({
 
     <div className="h-screen flex">
       {/* left */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200"> L </div>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+        <Link href="/" className="flex items-center justify-center lg:justify-start gap-2 ">
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">School Management</span>
+        </Link>
+        <Menu/>
+      </div>
       {/* right */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%]xl:w-[86%] bg-blue-200"> R </div>
     </div>
