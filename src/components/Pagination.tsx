e6fe765f2927268c4238f91fcd79ec627ac56e89
Youssef <yousseftahertemraz.com>
2025-03-14 22:56:50 +0200

    enable user to navigate through pages and control the change in url for only page

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,6 +1,17 @@
+"use client"
+
 import { ITEMS_PER_PAGE } from "@/lib/settings"
+import { useRouter } from "next/navigation";
 
 const Pagination = ({ page, count }: { page: number, count: number }) => {
+
+const router = useRouter()
+// change only the page number not other params on the url
+const changePage = (newPage: number) => {
+    const params = new URLSearchParams(window.location.search);
+    params.set("page", newPage.toString());
+    router.push(`${window.location.pathname}?${params}`);
+  };
     return (
         <div className='p-4 flex items-center justify-between text-gray-500'>
             <button disabled className="px-4 py-2 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">Prev</button>
@@ -12,8 +23,10 @@ const Pagination = ({ page, count }: { page: number, count: number }) => {
                         return (
                             <button
                                 key={pageIndex}
-                                className={`px-2 rounded-sm ${page === pageIndex? "bg-lamaSky" :""}`}>
-                                    {pageIndex}
+                                className={`px-2 rounded-sm ${page === pageIndex ? "bg-lamaSky" : ""}`}
+                                onClick={()=>{changePage(pageIndex)}}
+                                >
+                                {pageIndex}
                             </button>
                         )
                     })
